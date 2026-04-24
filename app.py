92caca8814d83b0ac1f1a6115cb92ab113d94958
Vachaspati Diwevedi <vachaspati.diwevedi@cumulocity.com>
2026-04-24 03:59:12 +0000

    fixed the tedge MQTT access URL

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ log.info("Log rotation setup successfully!")
 # Configuration
 CONFIG = {
     "PI_DATAPOINTS_FILE": "/etc/tedge/c8y/datapoints.json",
-    "MQTT_BROKER": "host.docker.internal",
+    "MQTT_BROKER": "host.containers.internal",
     "MQTT_PORT": 1883,
     "HTTP_PORT": 8001,
     "MQTT_MEASUREMENT_TOPIC": "c8y/measurement/measurements/create",
