8dd89cee874fabaa7d772c7571335100465bc7fe
Vachaspati Diwevedi <vachaspati.diwevedi@cumulocity.com>
2026-09-03 16:35:51 +0530

    fixed MQTT URL for docker

--- app.py
+++ app.py
@@ -32,7 +32,8 @@ log.info("Log rotation setup successfully!")
 # Configuration
 CONFIG = {
     "PI_DATAPOINTS_FILE": "/etc/tedge/c8y/datapoints.json",
-    "MQTT_BROKER": "host.containers.internal",
+    #"MQTT_BROKER": "host.containers.internal",
+    "MQTT_BROKER": "localhost",
     "MQTT_PORT": 1883,
     "HTTP_PORT": 8001,
     "MQTT_MEASUREMENT_TOPIC": "c8y/measurement/measurements/create",
